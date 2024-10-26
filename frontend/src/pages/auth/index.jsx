5e776fa59f86246153a2dd8fe84235bf4bcea1bb
Kushal3502 <kushal2002personal@gmail.com>
2024-10-26 19:58:08 +0530

    deleted unnecessary code

--- frontend/src/pages/auth/index.jsx
+++ frontend/src/pages/auth/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Form from "@/components/form/Form";
 import { SigninController, SignupController } from "@/config";
@@ -9,9 +9,18 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { AuthContext } from "@/context/authContext";
 
 function Auth() {
   const [activeTab, setActiveTab] = useState("signin");
+  const {
+    signInFormData,
+    setSignInFormData,
+    signUpFormData,
+    setSignUpFormData,
+  } = useContext(AuthContext);
+
+  console.log(signInFormData);
 
   return (
     <div className="w-full min-h-screen flex justify-center items-center  text-white">
@@ -32,26 +41,40 @@ function Auth() {
         <TabsContent value="signin">
           <Card className="bg-zinc-950 text-white border-zinc-700">
             <CardHeader>
-              <CardTitle className="text-2xl font-normal">Sign in to your account</CardTitle>
+              <CardTitle className="text-2xl font-normal">
+                Sign in to your account
+              </CardTitle>
               <CardDescription>
                 Enter your email and password to access your account
               </CardDescription>
             </CardHeader>
             <CardContent className="space-y-2">
-              <Form formControl={SigninController} buttonText="Sign in" />
+              <Form
+                formControl={SigninController}
+                buttonText="Sign in"
+                formData={signInFormData}
+                setFormData={setSignInFormData}
+              />
             </CardContent>
           </Card>
         </TabsContent>
         <TabsContent value="signup">
           <Card className="bg-zinc-950 text-white border-zinc-700">
             <CardHeader>
-              <CardTitle className="text-2xl font-normal">Create a new account</CardTitle>
+              <CardTitle className="text-2xl font-normal">
+                Create a new account
+              </CardTitle>
               <CardDescription>
                 Enter your details to get started
               </CardDescription>
             </CardHeader>
             <CardContent className="space-y-2">
-              <Form formControl={SignupController} buttonText="Sign up" />
+              <Form
+                formControl={SignupController}
+                buttonText="Sign up"
+                formData={signUpFormData}
+                setFormData={setSignUpFormData}
+              />
             </CardContent>
           </Card>
         </TabsContent>
