beafede07f58b65c6554a6b0451eb446b6d46189
Kushal3502 <kushal2002personal@gmail.com>
2024-10-28 11:11:58 +0530

    minor changes

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -8,15 +8,15 @@ function Navbar() {
   console.log(user);
 
   return (
-    <div className=" w-full h-14 flex justify-between items-center px-6 py-3 border-b-2 border-zinc-300">
+    <div className=" w-full h-18 flex justify-between items-center px-10 py-3 border-b-2 border-zinc-300">
       <h2 className=" text-3xl font-bold flex items-center gap-2">
         {" "}
         <GraduationCap className="w-8 h-8" />
         Pathshala
       </h2>
       <div className=" flex items-center gap-4">
-        <p className="hidden md:block font-semibold">
-          Welcome, <span className=" text-xl ">{user.name}</span>
+        <p className="hidden md:block">
+          Welcome, <span className=" text-2xl ">{user.name}</span>
         </p>
         <Avatar>
           <AvatarImage src="https://github.com/shadcn.png" />
